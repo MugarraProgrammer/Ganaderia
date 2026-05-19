7f82bbedeaa16bfbd6ee7617c1f2595d9ca1f0a0
Esteban <es.mugarra@duocuc.cl>
2026-05-19 18:43:57 -0400

    Primer commit

---
+++ src/main/java/com/duocuc/ganaderia/GanaderiaApplication.java
@@ -0,0 +1,13 @@
+package com.duocuc.ganaderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanaderiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanaderiaApplication.class, args);
+	}
+
+}
