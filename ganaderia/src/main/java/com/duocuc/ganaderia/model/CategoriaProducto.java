058b2688a20b65a9db595e1915e31381d4f971d8
Esteban <es.mugarra@duocuc.cl>
2026-05-19 22:14:11 -0400

     creacion de entidades JPA del modelo (CategoriaProducto y Producto)

--- ganaderia/src/main/java/com/duocuc/ganaderia/model/CategoriaProducto.java
+++ ganaderia/src/main/java/com/duocuc/ganaderia/model/CategoriaProducto.java
@@ -1,5 +1,28 @@
 package com.duocuc.ganaderia.model;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categoria_producto")
+
 public class CategoriaProducto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank @Size(max = 100) @Column(nullable = false, length = 100)
+    private String nombre;
+    @Size(max = 255) @Column(length = 255)
+    private String descripcion;
 }
--- ganaderia/src/main/java/com/duocuc/ganaderia/model/Producto.java
+++ ganaderia/src/main/java/com/duocuc/ganaderia/model/Producto.java
@@ -1,5 +1,39 @@
 package com.duocuc.ganaderia.model;
 
-public class Producto {
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "producto_carnes")
+
+public class Producto {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 120)
+    private String nombre;
+    @Column(nullable = false, unique = true, length = 50)
+    private String codigo;
+    @Column(nullable = false, precision = 10, scale = 0)
+    private BigDecimal precio;
+    @Column(length = 80)
+    private String marca;
+    @Column(length = 80)
+    private String presentacion;
+    @Column(length = 255)
+    private String notas;
+    @Column(name = "fecha_vencimiento")
+    private LocalDate fechaVencimiento;
+    @ManyToOne(optional = false) @JoinColumn(name = "categoria_id", nullable = false)
+    private CategoriaProducto categoria;
+    
 }
